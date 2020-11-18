654a89a2b3749e300c91a194095930ff0be2704e
mpeyre93 <marlee.eyre@gmail.com>
2020-11-18 00:31:19 +0000

    reservation form added to make.html

--- script.js
+++ script.js
@@ -1,7 +1,57 @@
 var express = require ('express');
-var port = 3000;
+var bodyParser = require('body-parser');
+var path = require('path');
 
 var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+
 var customers = [];
 var waitlist = [];
 
+var tables = [
+  {
+    name: ''
+    phone:''
+    email:''
+    customerOrder: 1
+  },
+  {
+    name:''
+    phone:''
+    email:''
+    customerOrder:2 
+  },
+  {
+    name:''
+    phone:''
+    email:''
+    customerOrder:3 
+  },
+  {
+    name:''
+    phone:''
+    email:''
+    customerOrder:4 
+  },
+  {
+    name:''
+    phone:''
+    email:''
+    customerOrder:5
+  }
+
+]
+
+function isBooked (){
+  if (tables.length <=5) {
+    isBooked = true;
+    }else{
+      isBooked = false;
+    }
+  res.json(isBooked);
+}
+
+app.
